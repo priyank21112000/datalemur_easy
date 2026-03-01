2622ef9e024b5bd5a956d71d7d6846fe46193150
Priyank Sanghvi <47595788+priyank21112000@users.noreply.github.com>
2026-02-28 19:11:29 -0500

    Create Laptop vs. Mobile Viewership_NYtimes.sql

---
+++ Laptop vs. Mobile Viewership_NYtimes.sql
@@ -0,0 +1,16 @@
+/*This is the same question as problem #3 in the SQL Chapter of Ace the Data Science Interview!
+
+Assume you're given the table on user viewership categorised by device type where the three types are laptop, tablet, and phone.
+
+Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership. Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views.
+
+*/
+
+-- LOGIC - CASE WHEN
+-- GRAIN - One row = entire dataset summarized into two buckets
+SELECT 
+
+SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+SUM(CASE WHEN device_type IN ('phone','tablet') THEN 1 ELSE 0 END) AS mobile_views
+
+FROM viewership
